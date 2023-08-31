9be192d06511e7d553c4e4fc1f7cfcfd57badac9
khizaransari <khizaransari721@gmail.com>
2023-09-01 01:22:05 +0500

    code improvements

--- app/Jobs/ScrapeMoviesJob.php
+++ app/Jobs/ScrapeMoviesJob.php
@@ -17,31 +17,11 @@ class ScrapeMoviesJob implements ShouldQueue
 
     public $tries = 3;
     public $retryAfter = 60;
-
     public function handle()
     {
         try {
-            $crawler = Goutte::request('GET', Config::get('app.movies.imdb_url') . '/chart/top');
-            $movies = $crawler->filter('.ipc-metadata-list li')->slice(0, 10)->each(function ($node) {
-                $title = $node->filter('.ipc-title__text')->text();
-                $year = $node->filter('.cli-title-metadata-item')->eq(0)->text();
-                // Extract the numeric rating
-                $rating = $node->filter('.ipc-rating-star--imdb')->attr('aria-label');
-                $rating = floatval(preg_replace('/[^0-9.]/', '', $rating));
-                $url = 'https://www.imdb.com' . $node->filter('.ipc-title-link-wrapper')->attr('href');
-                return compact('title', 'year', 'rating', 'url');
-            });
-
-            foreach ($movies as $movie) {
-                Movie::updateOrCreate(
-                    ['title' => $movie['title']],
-                    [
-                        'year' => $movie['year'],
-                        'rating' => $movie['rating'],
-                        'url' => $movie['url'],
-                    ]
-                );
-            }
+            $movies = $this->scrapeMoviesFromIMDb();
+            $this->saveMoviesToDatabase($movies);
         } catch (RequestException $e) {
             report($e);
             throw new \Exception('Failed to connect to IMDb. Please try again later.');
@@ -49,4 +29,33 @@ class ScrapeMoviesJob implements ShouldQueue
             throw $e;
         }
     }
+
+    private function scrapeMoviesFromIMDb()
+    {
+        $crawler = Goutte::request('GET', Config::get('app.movies.imdb_url') . '/chart/top');
+        return $crawler->filter('.ipc-metadata-list li')->slice(0, 10)->each(function ($node) {
+            $title = $node->filter('.ipc-title__text')->text();
+            $year = $node->filter('.cli-title-metadata-item')->eq(0)->text();
+            // Extract the numeric rating
+            $rating = $node->filter('.ipc-rating-star--imdb')->attr('aria-label');
+            $rating = floatval(preg_replace('/[^0-9.]/', '', $rating));
+            $url = 'https://www.imdb.com' . $node->filter('.ipc-title-link-wrapper')->attr('href');
+            return compact('title', 'year', 'rating', 'url');
+        });
+    }
+
+    private function saveMoviesToDatabase($movies)
+    {
+        foreach ($movies as $movie) {
+            Movie::updateOrCreate(
+                ['title' => $movie['title']],
+                [
+                    'year' => $movie['year'],
+                    'rating' => $movie['rating'],
+                    'url' => $movie['url'],
+                ]
+            );
+        }
+    }
+
 }
